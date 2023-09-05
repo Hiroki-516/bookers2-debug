a729f58db0b1b879798d333851cd06e3cc37555f
Hiroki-516 <141198036+Hiroki-516@users.noreply.github.com>
2023-09-05 14:00:25 +0000

    応用課題4 修正版

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:update, :edit]
-
-  #フォロー一覧
-  def follows
-    user = User.find(params[:id])
-    @users = user.following_users
-  end
-
-  def followers
-    user = User.find(params[:id])
-    @user = user.follower_users
-  end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    @following_users = @user.following_users
-    @follower_users = @user.follower_users
   end
 
   def index
